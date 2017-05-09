8b899ebe49b99c979dde3674eb4957cc58c2e6fe
dxs <glaglasven@live.com>
2017-05-09 09:44:12 +0200

    Set product

---
+++ Qoqa/Logic/GetMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qoqa.Logic
+{
+	public class GetMain
+	{
+		string baseURL = @"http://qoqa.ch";
+		string page = string.Empty;
+		public Product Product = new Product();
+
+		public GetMain()
+		{
+			GetPage();
+		}
+
+		private async void GetPage()
+		{
+			HttpClient client = new HttpClient();
+			page = await client.GetStringAsync(baseURL);
+			ParsePageToProduct(page);
+		}
+
+		private void ParsePageToProduct(string page)
+		{
+			
+		}
+	}
+}
---
+++ Qoqa/Logic/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Qoqa.Logic
+{
+	public class Product : INotifyPropertyChanged
+	{
+		private string price = string.Empty;
+		public string Price
+		{
+			get { return price; }
+			set { price = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Price))); }
+		}
+
+		private string nrlprice = string.Empty;
+		public string NrlPirce
+		{
+			get { return nrlprice; }
+			set { nrlprice = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NrlPirce))); }
+		}
+
+		private string name = string.Empty;
+		public string Name
+		{
+			get { return name; }
+			set { name = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name))); }
+		}
+
+		private BitmapImage image = new BitmapImage();
+		public BitmapImage Image
+		{
+			get { return image; }
+			set { image = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Image))); }
+		}
+
+		private string description = string.Empty;
+		public string Description
+		{
+			get { return description; }
+			set { description = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Description))); }
+		}
+
+		public Product()
+		{
+
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
