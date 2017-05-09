e5f0dba8d0b568d6d69c47b78a1a7f7e0516f244
dxs <glaglasven@live.com>
2017-05-09 11:58:07 +0200

    backdrop push

--- Qoqa/MainPage.xaml.cs
+++ Qoqa/MainPage.xaml.cs
@@ -3,13 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
@@ -24,6 +30,30 @@ namespace Qoqa
         public MainPage()
         {
             this.InitializeComponent();
+			applyAcrylicAccent(BackdropGrid);
         }
-    }
+
+		private void applyAcrylicAccent(Panel e)
+		{
+			_compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
+			_hostSprite = _compositor.CreateSpriteVisual();
+			_hostSprite.Size = new Vector2((float)BackdropGrid.ActualWidth, (float)BackdropGrid.ActualHeight);
+
+			ElementCompositionPreview.SetElementChildVisual(
+					BackdropGrid, _hostSprite);
+			_hostSprite.Brush = _compositor.CreateHostBackdropBrush();
+			ApplicationViewTitleBar formattableTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+			formattableTitleBar.ButtonBackgroundColor = Colors.Transparent;
+			CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+			coreTitleBar.ExtendViewIntoTitleBar = true;
+		}
+		Compositor _compositor;
+		SpriteVisual _hostSprite;
+
+		private void BackdropGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			if (_hostSprite != null)
+				_hostSprite.Size = e.NewSize.ToVector2();
+		}
+	}
 }
